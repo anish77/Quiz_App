ab911c2e4832682d1d93a6a2cf91b4906df362da
Ana Cvasniuc <annycvasniuc@yahoo.com>
2024-08-20 11:47:05 +0200

    first push, need to check the bug present

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
+
+void main() {
+  //runApp(const MaterialApp(home: QuizApp(),));
+  runApp(const Quiz());
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> _selectedAnswers = [];
+  var _activeScreen = 'start-screen';
+  //Widget? activeScreen; // prima soluzione
+  /*
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }*/
+
+  void switchScreen() {
+    setState(() {
+      _activeScreen = 'questions-screen'; //const QuestionsScreen();
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    _selectedAnswers.add(answer);
+
+    if (_selectedAnswers.length == questions.length) {
+      setState(() {
+        // selectedAnswers = [];
+        _activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      _activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    /* final screenWidget = activeScreen == 'start-screen'
+        ? StartScreen(switchScreen)
+        : const QuestionsScreen();  oppure   */
+    Widget screenWidget = StartScreen(switchScreen);
+    
+    if (_activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+
+    if (_activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: _selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 175, 82, 229),
+                Color.fromARGB(255, 113, 8, 151),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          // child: activeScreen,
+          child: screenWidget,
+        ), // Scaffold può essere inclusso sia qui oppure in StartScreen
+      ),
+    );
+  }
+}
